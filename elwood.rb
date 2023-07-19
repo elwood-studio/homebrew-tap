00a720a27cdd6ead215f2426f1ce6dd76625629f
@traviskuhl <security@elwood.studio>
2023-07-19 21:36:02 +0000

    Update release

--- elwood.rb
+++ elwood.rb
@@ -1,21 +1,21 @@
 class Elwood < Formula
   desc "Elwood CLI"
   homepage "https://elwood.studio/docs/cli"
-  version "0.1.35"
+  version "0.1.36"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/elwood-studio/cli/releases/download/0.1.35/elwood-macos-x64.zip"
-      sha256 "765602d217e33e957782b068b9fae4b3a3d3fab75585d7c4920e8634f99baae5"
+      url "https://github.com/elwood-studio/cli/releases/download/0.1.36/elwood-macos-x64.zip"
+      sha256 "d6559a9be3ee4f5bfb21e477678da6981b075d0148311c8cf15aa7ae8db6bc53"
 
       def install
         bin.install "elwood"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/elwood-studio/cli/releases/download/0.1.35/elwood-macos-x64.zip"
-      sha256 "765602d217e33e957782b068b9fae4b3a3d3fab75585d7c4920e8634f99baae5"
+      url "https://github.com/elwood-studio/cli/releases/download/0.1.36/elwood-macos-x64.zip"
+      sha256 "d6559a9be3ee4f5bfb21e477678da6981b075d0148311c8cf15aa7ae8db6bc53"
 
       def install
         bin.install "elwood"
@@ -25,16 +25,16 @@ class Elwood < Formula
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/elwood-studio/cli/releases/download/0.1.35/elwood-linux-arm64.zip"
-      sha256 "4d3cc0e3b507554be3fe536a6ee094a095ef8e5b82252be2b37faade80de0304"
+      url "https://github.com/elwood-studio/cli/releases/download/0.1.36/elwood-linux-arm64.zip"
+      sha256 "5ac304ffdbffddb7b3fb29d69cfdf1739061dd664e5ee07fc48e52aa2a84a57b"
 
       def install
         bin.install "elwood"
       end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/elwood-studio/cli/releases/download/0.1.35/elwood-linux-x64.zip"
-      sha256 "46edc66b4e9578b0183cd065eaa66f47847e1f4efb9a163ea9cfe4bf30412f5a"
+      url "https://github.com/elwood-studio/cli/releases/download/0.1.36/elwood-linux-x64.zip"
+      sha256 "b975a85b52f746545424332bfae9c21e49484fcfaa685b61ac94966bc73d737a"
 
       def install
         bin.install "elwood"
